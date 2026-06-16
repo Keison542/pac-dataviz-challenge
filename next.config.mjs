13079b3de5c2293ed7214b301e0d65f4d42d5930
Keison542 <82827605+Keison542@users.noreply.github.com>
2026-06-16 00:09:39 +0000

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
+const repo = "pac-dataviz-challenge";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
   images: {
     unoptimized: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
