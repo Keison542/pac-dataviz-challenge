a86d05473f7062d4207f0f87ffdcc7444a0bf4d3
Keison542 <82827605+Keison542@users.noreply.github.com>
2026-07-02 22:46:43 +1000

    Update PacificMap.tsx

--- src/components/PacificMap.tsx
+++ src/components/PacificMap.tsx
@@ -709,9 +709,9 @@ export function PacificClimateStoryMap({ data, selectedCountry, className = "" }
         </p>
       </div>
 
-       <p>Fig 6: The composite vulnerability level across the Pacific, inclluding impact size of cyclone, flood, drought and rise in sea level</p>
-        </br>
-      <p>{narrativeText</p>
+     <p>Fig 6: The composite vulnerability level across the Pacific, including impact size of cyclone, flood, drought and rise in sea level</p>
+      <br />
+      <p>{narrativeText}</p>
 
        {/* ─── NARRATIVE STORY ─── */}
       {narrativeText && (
